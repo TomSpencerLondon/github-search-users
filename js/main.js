52a3e8e818655928af3c099430fb052acef49135
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-01 20:12:58 +0000

    return user object from github API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   console.log("Ready...")
+  $('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
 
-})
+    // Make a request to Github
+    $.ajax({
+      url:'https://api.github.com/users/' +username,
+      data:{
+        client_id:'9983f7a2019397d3bded',
+        client_secret: '3fd3b2f14f3d1034bee4b5fe135160bc46e86c7d'
+      }
+    }).done(function(user){
+        console.log(user)
+      });
+});
